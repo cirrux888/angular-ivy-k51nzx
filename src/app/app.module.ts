642bf5accf3c5d66f39276363b3de1de8f1d2cf1
cirrux888 <quinn.wong@cirruxsolutions.com>
2020-05-11 22:36:04 -0700

    Added MatRadioModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatCheckboxModule ],
+  imports:      [ BrowserModule, FormsModule, MatCheckboxModule, MatRadioModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
